bcce4dac7b2043317a7bdb34d15674e47e756aa6
Faizyab Khalid <129151546+Faiz510@users.noreply.github.com>
2024-05-29 17:21:54 +0500

    Connect App To Database
    - Connecting app to database .
    - install necessary package for connection mongodb , mongoose , express and dotenv

--- backend/server.ts
+++ backend/server.ts
@@ -1,1 +1,16 @@
-console.log("server js");
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import app from "./app";
+
+dotenv.config();
+
+const DB: string = process.env.DATABASE || "";
+const PORT = process.env.PORT || 3000;
+
+if (!DB) throw new Error("database is not availble");
+
+mongoose.connect(DB).then(() => console.log("connected to database"));
+
+app.listen(PORT, () => {
+  console.log(`app listen to port ${PORT}`);
+});
