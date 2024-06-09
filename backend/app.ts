f6e593720cb8cf544e7811fb9c0206b822b79b5a
Faizyab Khalid <129151546+Faiz510@users.noreply.github.com>
2024-06-09 09:01:09 +0500

    Connect Backend To Frontend
    - Connect backend to frontend
    - Install CORS package at backend for cross origin access
    - Add base_url to frontend env to store backend base url to
     env

--- backend/app.ts
+++ backend/app.ts
@@ -1,23 +1,31 @@
-import express from "express";
-import authRoute from "./routes/authRoute";
-import boardRoute from "./routes/boardRoute";
-import taskRoute from "./routes/tasksRoute";
-import subTaskRoute from "./routes/subTaskRoute";
-import globalErrorHandler from "./utils/globalErrorHandler";
-import AppError from "./utils/AppError";
-import cookieParser from "cookie-parser";
+import express from 'express';
+import authRoute from './routes/authRoute';
+import boardRoute from './routes/boardRoute';
+import taskRoute from './routes/tasksRoute';
+import subTaskRoute from './routes/subTaskRoute';
+import globalErrorHandler from './utils/globalErrorHandler';
+import AppError from './utils/AppError';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
 
+app.use(
+  cors({
+    origin: `${process.env.FRONTEND_URL || 'http://localhost:5173'}`,
+    credentials: true,
+  }),
+);
+
 app.use(cookieParser());
 
-app.use("/api/v1/auth", authRoute);
-app.use("/api/v1/board", boardRoute);
-app.use("/api/v1/task", taskRoute);
-app.use("/api/v1/sub-task", subTaskRoute);
+app.use('/api/v1/auth', authRoute);
+app.use('/api/v1/board', boardRoute);
+app.use('/api/v1/task', taskRoute);
+app.use('/api/v1/sub-task', subTaskRoute);
 
-app.all("*", (req, res, next) => {
+app.all('*', (req, res, next) => {
   next(new AppError(400, `can not found ${req.originalUrl}`));
 });
 
